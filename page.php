b8478554efc75250df011787316643dbd687ebfe
Jamie Elliott <jamieelliott21@gmail.com>
2016-05-31 15:24:32 -0400

    Initial Commit

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header();  ?>
+
+<div class="main">
+  <div class="container">
+
+    <div class="content">
+      <?php // Start the loop ?>
+      <?php 
+      if(have_posts() ) {
+        while( have_posts()) {
+          the_post();
+          ?>
+          <h3><?php the_title();?></h3> 
+          <?php the_content();?>
+        <?php
+        }//closes the while loop
+        }//closes if statement
+        ?>
+    </div> <!-- /,content -->
+
+    <?php get_sidebar(); ?>
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,59 @@
+<?php get_header(); ?>
+
+<div class="main">
+  <div class="container">
+    <div class="content">
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+
+          <div class="entry-meta">
+
+              <?php 
+                 echo get_avatar( get_author_id(), $size = '96'); 
+                 ?>
+              <p>this post was written by: <?php the_author(); ?></p>
+              <p>This post was written on: <?php the_date(); ?> </p>
+          </div>
+          <pre>
+          <?php 
+              echo $post->ID;
+          ?>
+          </pre>
+                <img src="<?php echo hackeryou_featured_image_url($post); ?>" alt="">
+
+
+                <?php //the_post_thumbnail('thumbnail'); ?>
+
+          <div class="entry-content">
+            <?php the_content(); ?>
+            <?php wp_link_pages(array(
+              'before' => '<div class="page-link"> Pages: ',
+              'after' => '</div>'
+            )); ?>
+          </div><!-- .entry-content -->
+
+          <div class="entry-utility">
+            <?php hackeryou_posted_in(); ?>
+            <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+          </div><!-- .entry-utility -->
+        </div><!-- #post-## -->
+
+        <div id="nav-below" class="navigation">
+          <p class="nav-previous"><?php previous_post_link('%link', '&larr; %title'); ?></p>
+          <p class="nav-next"><?php next_post_link('%link', '%title &rarr;'); ?></p>
+        </div><!-- #nav-below -->
+
+        <?php comments_template( '', true ); ?>
+
+      <?php endwhile; // end of the loop. ?>
+
+    </div> <!-- /.content -->
+
+    <?php get_sidebar(); ?>
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
